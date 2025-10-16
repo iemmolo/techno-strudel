c15dd8717b26d98b6dc606d9ff553a42df74a5a8
Jack Iemmolo <iemmolo@users.noreply.github.com>
2025-10-16 21:40:09 +0800

    Finish Song

--- techno.js
+++ techno.js
@@ -1,51 +1,82 @@
+////////////////////////////////////////////////////////////
+// -- Custom Functions Import --
+// eval(await(await fetch('https://raw.githubusercontent.com/switchangel/strudel-scripts/main/allscripts.js')).text());
+////////////////////////////////////////////////////////////
 samples('github:switchangel/pad')
-
 
 setCps(140/60/4)
 
 
 let supersaw = n(irand(rand.range(0,10)).sound("supersaw")
   .sub(rand).seg(16))
-  // .scale("c:minor g:minor")
-  // .add("<100>")
   .lpenv(4)
   .crush(9)
-  .lpf(100)
+  .lpf(300)
   .slow(1)
 
-let supersaw2 = n(irand(rand.range(0,10)).sound("supersaw")
+let supersaw2 = n(irand(rand.range(0,5)).sound("saw")
   .sub(rand).seg(16))
-  .scale("c:dorian")
-  // .echo(1,1, 2)
-  // .add("100")
-  // .lpenv(4)
-  // .crush(7)
-  .lpf("<300 600>")
+  .crush(9)
+  .lpf("<1000>")
   .slow(1)
   .delay(2)
-  // .gain(.7)
+  .gain(.7)
 
-let supersaw3 = n(irand(rand.range(0,10)).sound("supersaw")
-  .sub(rand).seg(16))
-  .scale("c:dorian")
-  // .echo(1,1, 2)
-  // .add("100")
-  // .lpenv(4)
-  // .crush(7)
-  .lpf("<300 400>")
-  .slow(1)
-  .delay(1)
-  // .gain(.7)
+let music = n("<[0 3 7 5] [6 8 10 15] [0 3 7 5] [12 10 8 6]>").sound("sawtooth")
+  .scale("c:phrygian")
+  // .lpf(sine.range(200, 8800).slow(4))
+  .lpq(10)
+  .every(4, x => x.set(n("[6 8 10 15]")))
+  .gain(0.8)
+  .room(0.3)
+  .cutoff(2000)
+  .fit().chop("<3 4>")
 
+// let music2= n("<[0 3 7 10 12 10 7 3] [5 8 12 15 17 15 12 8] [3 7 10 14 15 14 10 7] [0 3 7 10 12 15 17 19]>").sound("supersaw")
+let music2= s("~ ~ supersaw ~ ~ ~ supersaw ~").n("<[0 3 7 10 12 10 7 3] [5 8 12 15 17 15 12 8] [3 7 10 14 15 14 10 7] [0 3 7 10 12 15 17 19]>").sound("supersaw")
+  .scale("c:minor")
+  .cutoff(2000).fit().chop(12)
+  // .lpf(sine.range(200, 800).slow(4))
+  .lpq(10)
+  .gain(0.8)
+  .room(0.3)
+  .slow(2)
+
+
+let perc = s("~ cp ~ cp").bank("RolandTR808")
+  .delay(0.5)
+  .lpf(sine.range(600, 800).slow(4))
+  .gain(0.7)
 
 let bd = s("[bd*2*2] [bd*2*2, bd*2*2 bd*2*2]").bank("RolandTR808")
 
-let piano =  n("[2, 2] [3 3]").add("< e f g >").sound("piano")
+let hats = s("hh*16").bank("RolandTR808")
+  .gain("1 0.5 0.7 0.5")
+  .speed(rand.range(0.95, 1.05))
+  .hpf(9000)
+  .gain(0.4)
+
+let drone = note("c1").s("sawtooth")
+  .lpf(150)
+  .gain(0.4)
+  .room(0.8)
+  .slow(2)
+
+
 
 stack(
-  // supersaw,
+  drone,
+  bd,
+  perc,
+  hats,
+  supersaw,
   supersaw2,
-  // bd,
-  // supersaw3,
-  // piano
-)._scope()
+  music,
+  music2,
+).color("violet").scope()
+// -- Jack --
+
+
+// -- Video reference  --
+// -- https://www.youtube.com/watch?v=v-bo4HxIMbQ&t --
+/////////////////////////////////////////////////////////////////////////////
